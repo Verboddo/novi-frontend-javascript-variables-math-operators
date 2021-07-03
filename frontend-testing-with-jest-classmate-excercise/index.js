f5ff53a33df148222effb8a8dd280a14e7b264b5
Verboddo <verboddo@gmail.com>
2021-07-03 15:08:18 +0200

    Write my code

--- frontend-testing-with-jest-classmate-excercise/index.js
+++ frontend-testing-with-jest-classmate-excercise/index.js
@@ -1,10 +1,5 @@
 function strongestPokemon(pokemon, strongest) {
-    for (let i = 0; i < pokemon.length; i++) {
-        const currentPokemon = pokemon[i]
-        if ( currentPokemon.pokemon === strongest ) {
-            return currentPokemon
-        }
-    }
+    // WRITE ME
 }
 
 module.exports = {
